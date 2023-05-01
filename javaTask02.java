b8c838508727e14002c6a5407a8fcaa167d1b200
bongs0625 <132265122+bongs0625@users.noreply.github.com>
2023-05-01 23:43:08 +0900

    Create javaTask02.java

---
+++ javaTask02.java
@@ -0,0 +1,22 @@
+# ZeroBase_BackEndSchool_Task
+/*
+백엔드_스쿨_13기_임근봉
+과제02. 결제 금액 캐시백 계산 프로그램
+*/
+
+public static void main(String[] args) {
+    System.out.println("[캐시백 계산]");
+    System.out.print("결제 금액을 입력해 주세요.(금액) : ");
+    Scanner myScan = new Scanner(System.in);
+    int cashInput = myScan.nextInt();
+    int cashBack;
+
+    if (cashInput < 1000) {
+        cashBack = 0;
+    } else if (cashInput < 3000) {
+        cashBack = (cashInput / 1000) * 100;
+    } else {
+        cashBack = 300;
+    }
+    System.out.println("결제 금액은 " + cashInput + "원 이고, 캐시백은 " + cashBack + "원 입니다.");
+}
