bd98863e86efd488d92ce6d9ebc869a4a0810500
bongs0625 <132265122+bongs0625@users.noreply.github.com>
2023-05-01 23:37:41 +0900

    Update javaTask01.java

--- javaTask01.java
+++ javaTask01.java
@@ -5,17 +5,15 @@
 String.format()을 활용한 구구단 출력
 */
 
-public class task {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        System.out.println("[ 구구단 출력 ]");
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                System.out.print(String.format("%02d", i) + " x ");
-                System.out.print(String.format("%02d", j) + " = ");
-                System.out.print(String.format("%02d", i * j) + "   ");
-            }
-            System.out.println();
+    System.out.println("[ 구구단 출력 ]");
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
+            System.out.print(String.format("%02d", i) + " x ");
+            System.out.print(String.format("%02d", j) + " = ");
+            System.out.print(String.format("%02d", i * j) + "   ");
         }
+        System.out.println();
     }
 }
